78529e3012a7ba9cc832ad8182237807002f8c07
Mustafa ilahi <mustafailahi586@gmail.com>
2024-09-08 16:25:28 +0500

    updated navigation

--- src/components/BackButton/index.js
+++ src/components/BackButton/index.js
@@ -2,11 +2,14 @@ import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
 import {images} from '../../services/utilities/images';
 import {sizes} from '../../services';
+import {useNavigation} from '@react-navigation/native';
 
 export default function BackButton() {
+const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.goBack()}>
         <Image source={images.backIcon} style={styles.backIcon} />
       </TouchableOpacity>
     </View>
